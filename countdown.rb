0d049f9dd109d6204e72c35ee23721609fbaca3c
rickie <rickiemaweu@gmail.com>
2022-08-29 11:47:27 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,16 @@
 
 def countdown(num)
 
-    while num>0
-      puts "#{num} SECOND(S)!"
-      num -=1
-      if num == 0
-       return 'HAPPY NEW YEAR!'
-      end
-  
+  while num>0
+    puts "#{num} SECOND(S)!"
+    num -=1
+    if num == 0
+     return 'HAPPY NEW YEAR!'
     end
+
+  end
 end
 
 def countdown_with_sleep(num)
-    sleep num
+  sleep num
 end
